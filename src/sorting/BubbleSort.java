8f50ca88cd781280b733be3b650b40f156a4eecd
Sai-dhruv <saikrishna.spark@gmail.com>
2022-10-05 06:37:21 -0400

    Bubble sort implemented

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -1,10 +1,23 @@
 package sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int a[] = {5,2,12,43,1,3,0};
+        for(int i =0;i<a.length-1;i++) {
+        	for(int j=0;j<a.length-1;j++) {
+        		if(a[j] > a[j+1]) {
+        			int temp = a[j];
+        			a[j] = a[j+1];
+        			a[j+1] = temp;
+        		}
+        	}
+        }
+		
+		System.out.println(Arrays.toString(a));
 	}
 
 }
